86e482668c49dee45286eebec2f986c4220ffe60
MartinXu1909 <martinxu1909@gmail.com>
2016-09-28 17:24:25 +0800

    使用do...while结构求0~100的整数数字之和

---
+++ Java 数据结构/DoWhile.java
@@ -0,0 +1,20 @@
+/* 
+使用do...while结构求0~100的整数数字之和 
+*/
+
+public class DoWhile{
+	public static void main(String[] args){
+		int limit = 100;
+		int sum = 0;
+		int i = 1;
+		do{
+			sum = sum + i;
+			i++;
+		}
+		while(i <= limit);
+		System.out.println("sum = " + sum);
+	}
+}
+/* 
+sum = 5050
+ */
