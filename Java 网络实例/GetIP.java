d9d83cefdc287e0e77dd99b43629921b64edd13e
MartinXu1909 <martinxu1909@gmail.com>
2016-09-30 20:01:38 +0800

    获取指定主机的IP地址

---
+++ Java 网络实例/GetIP.java
@@ -0,0 +1,22 @@
+/* 
+ 使用 InetAddress 类的 InetAddress.getByName() 方法来获取指定主机（网址）的IP地址
+*/
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class GetIP{
+	public static void main(String[] args){
+		InetAddress address = null;
+		try{
+			address = InetAddress.getByName("www.google.com");
+		}
+		catch(UnknownHostException e){
+			System.exit(2);
+		}
+		System.out.println(address.getHostName() + "=" + address.getHostAddress());
+		System.exit(0);
+	}
+}
+/* 
+www.google.com=220.255.2.153
+ */
